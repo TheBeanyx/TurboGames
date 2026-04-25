d98ef1e2debdad9766b1fa64103104340a47e15a
TheBeanyx <buzas.benedek.iskola@gmail.com>
2026-04-25 18:09:09 +0200

    Add list of games with IDs and icons

---
+++ list.games.js
@@ -0,0 +1,7 @@
+export const games = [
+    { id: 1, name: "Grand Theft Auto V", icon: "cover.grand_theft_auto_v.jpg" },
+    { id: 2, name: "The Witcher 3", icon: "cover.the_witcher_3.jpg" },
+    { id: 3, name: "Cyberpunk 2077", icon: "cover.cyberpunk_2077.jpg" },
+    { id: 4, name: "Minecraft", icon: "cover.minecraft.jpg" },
+    { id: 5, name: "League of Legends", icon: "cover.league_of_legends.jpg" }
+];
