23fdcbb5140550e725a7248404eaee7d087973f3
TheBeanyx <buzas.benedek.iskola@gmail.com>
2026-04-25 18:16:11 +0200

    Add multiple entries for existing games

--- list.games.js
+++ list.games.js
@@ -4,4 +4,14 @@ export const games = [
     { id: 3, name: "Cyberpunk 2077", icon: "cover.cyberpunk_2077.jpg" },
     { id: 4, name: "Minecraft", icon: "cover.minecraft.jpg" },
     { id: 5, name: "League of Legends", icon: "cover.league_of_legends.jpg" }
+    { id: 5, name: "Grand Theft Auto V", icon: "cover.grand_theft_auto_v.jpg" },
+    { id: 6, name: "The Witcher 3", icon: "cover.the_witcher_3.jpg" },
+    { id: 8, name: "Cyberpunk 2077", icon: "cover.cyberpunk_2077.jpg" },
+    { id: 9, name: "Minecraft", icon: "cover.minecraft.jpg" },
+    { id: 10, name: "League of Legends", icon: "cover.league_of_legends.jpg" }
+    { id: 11,name: "Grand Theft Auto V", icon: "cover.grand_theft_auto_v.jpg" },
+    { id: 12, name: "The Witcher 3", icon: "cover.the_witcher_3.jpg" },
+    { id: 13, name: "Cyberpunk 2077", icon: "cover.cyberpunk_2077.jpg" },
+    { id: 14, name: "Minecraft", icon: "cover.minecraft.jpg" },
+    { id: 15, name: "League of Legends", icon: "cover.league_of_legends.jpg" }
 ];
