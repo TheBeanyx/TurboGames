1cc12b33ce55b529dc930292c8e9f974b5512916
TheBeanyx <buzas.benedek.iskola@gmail.com>
2026-04-25 18:17:02 +0200

    Replace game names with new placeholder values
    Updated game names in the list with new placeholders.

--- list.games.js
+++ list.games.js
@@ -4,14 +4,14 @@ export const games = [
     { id: 3, name: "Cyberpunk 2077", icon: "cover.cyberpunk_2077.jpg" },
     { id: 4, name: "Minecraft", icon: "cover.minecraft.jpg" },
     { id: 5, name: "League of Legends", icon: "cover.league_of_legends.jpg" }
-    { id: 5, name: "Grand Theft Auto V", icon: "cover.grand_theft_auto_v.jpg" },
-    { id: 6, name: "The Witcher 3", icon: "cover.the_witcher_3.jpg" },
-    { id: 8, name: "Cyberpunk 2077", icon: "cover.cyberpunk_2077.jpg" },
-    { id: 9, name: "Minecraft", icon: "cover.minecraft.jpg" },
-    { id: 10, name: "League of Legends", icon: "cover.league_of_legends.jpg" }
-    { id: 11,name: "Grand Theft Auto V", icon: "cover.grand_theft_auto_v.jpg" },
-    { id: 12, name: "The Witcher 3", icon: "cover.the_witcher_3.jpg" },
-    { id: 13, name: "Cyberpunk 2077", icon: "cover.cyberpunk_2077.jpg" },
-    { id: 14, name: "Minecraft", icon: "cover.minecraft.jpg" },
-    { id: 15, name: "League of Legends", icon: "cover.league_of_legends.jpg" }
+    { id: 5, name: "Gcvb", icon: "cover.grand_theft_auto_v.jpg" },
+    { id: 6, name: "Thevb", icon: "cover.the_witcher_3.jpg" },
+    { id: 8, name: "vc", icon: "cover.cyberpunk_2077.jpg" },
+    { id: 9, name: "Mifgt", icon: "cover.minecraft.jpg" },
+    { id: 10, name: "vb", icon: "cover.league_of_legends.jpg" }
+    { id: 11,name: "Grvb", icon: "cover.grand_theft_auto_v.jpg" },
+    { id: 12, name: "vfgh", icon: "cover.the_witcher_3.jpg" },
+    { id: 13, name: "Cdfg", icon: "cover.cyberpunk_2077.jpg" },
+    { id: 14, name: "Midfgt", icon: "cover.minecraft.jpg" },
+    { id: 15, name: "Lfg", icon: "cover.league_of_legends.jpg" }
 ];
